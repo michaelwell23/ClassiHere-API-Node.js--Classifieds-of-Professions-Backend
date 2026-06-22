f4fadfa884cc537feaadb6a5e2b78a5060d2d14e
michael wellington lopes <michael23.wellington@gmail.com>
2026-06-22 15:03:41 -0300

    feat: implement change password business rules

--- src/modules/auth/controllers/ChangePasswordController.js
+++ src/modules/auth/controllers/ChangePasswordController.js
@@ -3,7 +3,10 @@ const ChangePasswordService = require('../services/ChangePasswordService');
 class ChangePasswordController {
   async handle(request, response, next) {
     try {
-      const result = await ChangePasswordService.execute(request.validated.body);
+      const result = await ChangePasswordService.execute({
+        userId: request.user.id,
+        ...request.validated.body,
+      });
 
       return response.status(200).json(result);
     } catch (error) {
--- src/modules/auth/services/ChangePasswordService.js
+++ src/modules/auth/services/ChangePasswordService.js
@@ -1,7 +1,37 @@
+const AppError = require('../../../shared/errors/AppError');
+
+const UserRepository = require('../../users/repositories/UserRepository');
+
+const { compareHash, generateHash } = require('../../../shared/providers/hash/bcrypt.provider');
+
 class ChangePasswordService {
-  async execute() {
+  async execute({ userId, currentPassword, newPassword }) {
+    const user = await UserRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const passwordMatches = await compareHash(currentPassword, user.password);
+
+    if (!passwordMatches) {
+      throw new AppError('Current password is invalid', 400);
+    }
+
+    const samePassword = await compareHash(newPassword, user.password);
+
+    if (samePassword) {
+      throw new AppError('New password must be different from current password', 400);
+    }
+
+    const passwordHash = await generateHash(newPassword);
+
+    await UserRepository.update(user.id, {
+      password: passwordHash,
+    });
+
     return {
-      message: 'Change password service ready',
+      message: 'Password changed successfully',
     };
   }
 }
